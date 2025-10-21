6d1721e8ef904bb30b28673213860d414fe628aa
Subin Thapa <subinthapa2092@gmail.com>
2025-10-21 15:08:31 +0545

    updated new file

--- project1.py
+++ project1.py
@@ -1,1 +1,11 @@
 ##### python in a  program to do the work 
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def saying_hello():
+    return{
+        "Message":"Wellcome to My Website"
+    }
